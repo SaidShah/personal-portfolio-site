02e61b941eda3d0873eb781dc127ccaec382153f
said shah <saidmysr20@aol.com>
2019-03-26 18:38:29 -0400

    finished experience section

---
+++ src/components/Experience.js
@@ -0,0 +1,102 @@
+import React from 'react';
+import Zoom from 'react-reveal/Zoom';
+
+
+const Experience = () => (
+  <>
+  <div className="experience-row">
+
+<Zoom>
+  <div className="experience-div blue-bg blue-bg-mobile">
+  <h3 className="experience-h1">Java</h3>
+  <p className="experience-p">*  School Application - To be used by students and faculty for record keeping</p>
+  <p className="experience-p">*  Text Editor - Uses machine learning for self-generating text</p>
+  <p className="experience-p">*  Proficient with Advanced Data Structures</p>
+  <p className="experience-p">*  Proficient with Advanced Algorithms</p>
+  <p className="experience-p">*  Many more apps</p>
+  </div>
+</Zoom>
+
+<Zoom>
+  <div className="experience-div white-bg white-bg-mobile">
+  <h3 className="experience-h1">JavaScript</h3>
+  <p className="experience-p">*  Pong - Ping Pong Game, can be played 1 player or multi-player</p>
+  <p className="experience-p">*  SaidShah.com - My personal website</p>
+  <p className="experience-p">*  Ticker - Stock Market Simulator</p>
+  <p className="experience-p">*  Zain - English learning platform</p>
+  <p className="experience-p">*  Many more apps</p>
+  </div>
+</Zoom>
+
+<Zoom>
+  <div className="experience-div white-bg blue-bg-mobile">
+  <h3 className="experience-h1">React & Redux</h3>
+  <p className="experience-p">* ZaiN - Learn English Platform</p>
+  <p className="experience-p">* Ticker - Stock Market Simulation App</p>
+  <p className="experience-p">* Extreme Coding School - Coding bootcamp for kids and adults</p>
+  <p className="experience-p">* Auto Finder - Car Classifieds App</p>
+  <p className="experience-p">* Hogwartz - Harry Potter App</p>
+  </div>
+</Zoom>
+
+<Zoom>
+  <div className="experience-div blue-bg white-bg-mobile">
+  <h3 className="experience-h1">Ruby & Rails</h3>
+  <p className="experience-p">* WeMeet - clone of meetup app</p>
+  <p className="experience-p">* Pong - Ping Pong Game</p>
+  <p className="experience-p">* Mario Keys - Keyboard game featuring Super Mario Bros</p>
+  <p className="experience-p">* Ticker - Stock Market Simulator</p>
+  <p className="experience-p">* Many more apps</p>
+  </div>
+</Zoom>
+
+<Zoom>
+  <div className="experience-div blue-bg blue-bg-mobile">
+  <h3 className="experience-h1">Linux</h3>
+  <p className="experience-p">* Bash Shell</p>
+  <p className="experience-p">* Shell Scripting</p>
+  <p className="experience-p">* File IO</p>
+  <p className="experience-p">* Processes</p>
+  <p className="experience-p">* Shell Scripting</p>
+  </div>
+</Zoom>
+
+<Zoom>
+  <div className="experience-div white-bg white-bg-mobile">
+  <h3 className="experience-h1">Database</h3>
+  <p className="experience-p">* Created Entire College Database for keeping records of students</p>
+  <p className="experience-p">* Created Mechanic Shop Database for inventory and sales</p>
+  <p className="experience-p">* Created World Database - Countries, Populations, Languages, etc.</p>
+  <p className="experience-p">* Postgres, MySQL, Sqlite3 & Firebase</p>
+  <p className="experience-p">* Active Record</p>
+  </div>
+</Zoom>
+
+<Zoom>
+  <div className="experience-div white-bg blue-bg-mobile">
+  <h3 className="experience-h1">HTML5 & CSS3</h3>
+  <p className="experience-p">* Strong expertise in CSS3 & HTML5 - for mobile and desktop sites</p>
+  <p className="experience-p">* Sound knowledge of cross-browser compatibility & cross-platform testing</p>
+  <p className="experience-p">* Edit, design, and implement website graphics for both public and gated sites</p>
+  <p className="experience-p">* Used JavaScript when developing page navigation, form validation, time and date stamps</p>
+  <p className="experience-p">* Website Design</p>
+  </div>
+</Zoom>
+
+<Zoom>
+  <div className="experience-div blue-bg white-bg-mobile">
+  <h3 className="experience-h1">ASP.NET</h3>
+  <p className="experience-p">* Movie Star Web Application</p>
+  <p className="experience-p">* Validation Controls</p>
+  <p className="experience-p">* Data Source Controls</p>
+  <p className="experience-p">* Microsoft Data Servers</p>
+  <p className="experience-p">* Website Design</p>
+  <p className="experience-p">* Database Design and Manipulation</p>
+  </div>
+</Zoom>
+
+  </div>
+  </>
+);
+
+export default Experience;
--- src/components/Home.js
+++ src/components/Home.js
@@ -3,7 +3,9 @@ import welcome from '.././images/welcome-image.png'
 import Bounce from 'react-reveal/Bounce'
 import Progress from './Progress'
 import Flip from 'react-reveal/Flip'
+import Spin from 'react-reveal/Spin'
 import About from './About'
+import Experience from './Experience'
 
 
 const Home = () => (
@@ -18,8 +20,9 @@ const Home = () => (
   <div className="about" id="about">
       <About/>
   </div>
-  <div>
 
+
+  <div>
   <div className="row">
   <Flip right>
     <div className="column-left">
@@ -27,11 +30,21 @@ const Home = () => (
     </div>
   </Flip>
   </div>
-
   <Progress/>
-
-
   </div>
+
+
+  <div>
+  <div className="row" id="experience">
+    <div className="column-left">
+    <Spin>
+        <h1 className="bold-text">Experience</h1>
+    </Spin>
+    </div>
+  </div>
+    <Experience/>
+  </div>
+
   </>
 );
 
