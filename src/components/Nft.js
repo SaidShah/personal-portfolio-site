2f0fd1a72a0703f9115a1ee37252ad66209a9e05
Said Shah <Saidshahdev@gmail.com>
2022-05-06 01:34:30 -0400

    updated nft url

--- src/components/Nft.js
+++ src/components/Nft.js
@@ -3,7 +3,7 @@ import React from "react";
 const Nft = () => (
   <>
   <div style={{height: "700px"}}>
-    <iframe src="https://opensea.io/Tech-Buddy" style={{width: '100%', height: '100%'}}></iframe>
+    <iframe src="https://opensea.io/collection/air-cryptos?theme=dark&ref=0x0Dec784F9AE694AC1A7A2051Cbf1a62d1774978D" style={{width: '100%', height: '100%'}} title="air_cryptos_opensea"></iframe>
   </div>
   </>
 );
